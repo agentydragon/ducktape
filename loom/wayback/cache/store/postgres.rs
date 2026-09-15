7bf9aa3bba27847d8f5f9f0f548140587952aae0
Rai <agentydragon@gmail.com>
2026-09-14 18:36:37 -0700

    fix(wayback): adapt SeaORM calls to 2.0 API

--- loom/wayback/cache/store/postgres.rs
+++ loom/wayback/cache/store/postgres.rs
@@ -11,7 +11,7 @@ use sea_orm::sea_query::{Expr, OnConflict};
 use sea_orm::sqlx::postgres::PgListener;
 use sea_orm::{
     ColumnTrait, Condition, ConnectionTrait, Database, DatabaseConnection, DbBackend, EntityTrait,
-    QueryFilter, QueryOrder, Schema, Set, Statement, TryInsertResult,
+    ExprTrait, QueryFilter, QueryOrder, Schema, Set, Statement, TryInsertResult,
 };
 use serde_json::Value;
 use tokio::sync::broadcast;
@@ -150,28 +150,28 @@ impl PostgresArchiveStore {
             .if_not_exists()
             .to_owned();
         self.db
-            .execute(self.db.get_database_backend().build(&create_table))
+            .execute(&self.db.get_database_backend().build(&create_table))
             .await?;
         let create_table = schema
             .create_table_from_entity(metadata_record::Entity)
             .if_not_exists()
             .to_owned();
         self.db
-            .execute(self.db.get_database_backend().build(&create_table))
+            .execute(&self.db.get_database_backend().build(&create_table))
             .await?;
         let create_table = schema
             .create_table_from_entity(fill_lease::Entity)
             .if_not_exists()
             .to_owned();
         self.db
-            .execute(self.db.get_database_backend().build(&create_table))
+            .execute(&self.db.get_database_backend().build(&create_table))
             .await?;
         let create_table = schema
             .create_table_from_entity(fill_queue::Entity)
             .if_not_exists()
             .to_owned();
         self.db
-            .execute(self.db.get_database_backend().build(&create_table))
+            .execute(&self.db.get_database_backend().build(&create_table))
             .await?;
         Ok(())
     }
@@ -215,7 +215,7 @@ impl PostgresArchiveStore {
 
     async fn notify_fill_changed(&self, key: &FillLeaseKey) -> Result<()> {
         self.db
-            .execute(Statement::from_sql_and_values(
+            .execute(&Statement::from_sql_and_values(
                 DbBackend::Postgres,
                 "SELECT pg_notify($1, $2)",
                 [
