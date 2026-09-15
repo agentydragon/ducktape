89cdba2880e484124ceeb4f4992f43f50cdb0962
Rai <agentydragon@gmail.com>
2026-09-14 17:58:18 -0700

    fix(debundle): call HashMap::into_values directly

--- devinfra/js/debundle/identifier_rename_queue.rs
+++ devinfra/js/debundle/identifier_rename_queue.rs
@@ -209,7 +209,6 @@ pub fn compute_identifier_rename_queue(
     }
 
     let mut entries: Vec<RenameQueueEntry> = sites_by_name
-        .into_iter()
         .into_values()
         .flat_map(|sites| sites.into_iter())
         .map(|site| RenameQueueEntry {
